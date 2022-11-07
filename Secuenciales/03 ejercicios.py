f219be472126f0aa38e8b95293b091cb4cdee579
yorlin30 <yorlint45gmail.com~>
2022-11-06 20:12:33 -0500

    se estan subiendo 3 ejercicios de prueba

---
+++ Secuenciales/03 ejercicios.py
@@ -0,0 +1,21 @@
+#ejercicio 01 
+
+nota1 = int(input("Ingrese nota 01: "))
+nota2 = int(input("Ingrese nota 02: "))
+nota3 = int(input("Ingrese nota 03: "))
+print()
+prom = (nota1 + nota2 + nota3)/3
+print("El promedio e: ", prom)
+
+#ejercicio 02 
+
+''' 
+Desarrolle el programa que permite convertir una cantidad dada en metros a su equivalente en centímetros, pulgadas, pies y yardas. Considere los siguientes factores de conversión : 
+1 metro = 100 cm, 1 pie = 12 pulgadas, 1 yarda = 3 pies, 1 pulgada = 2.54 cm
+'''
+metros = int(input("Ingrese el numero de metros: "))
+pie = int(input("Ingrese los pie: "))
+yarda = int(input("Ingrese las yardas: "))
+pulgadas = int(input("Ingrese las pulgadas: "))
+print()
+
