bec2862dbe1cb92b8538a5ae708ca3d6858dca08
giluan13 <117103658+giluan13@users.noreply.github.com>
2022-12-16 20:02:59 -0500

    Add files via upload

---
+++ EJERCICIO 1.py
@@ -0,0 +1,9 @@
+def calcularArea(a, b):
+    area = (b* a) / 2 
+    return area
+print("Ingresar altura")
+a = float(input())
+print("ingresar base")
+b = float(input())
+
+print("El area es"  + str(calcularArea(a, b)))
