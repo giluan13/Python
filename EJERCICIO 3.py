561f05a3e6c0520040c81273608e9089afac16e6
giluan13 <117103658+giluan13@users.noreply.github.com>
2022-12-16 20:11:22 -0500

    Delete EJERCICIO 3.py

--- EJERCICIO 3.py
+++
@@ -1,8 +0,0 @@
-pi= 3.14159
-
-radioCono =float(input("Ingrese un valor para el cono: "))
-alturaCono =float(input("Ingrese la altura del cono: "))
-volumenCono=(1/3)*pi*(radioCono**2)*alturaCono
-print("El volumen del cono es: ",volumenCono)
-
-
