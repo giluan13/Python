222598c08b82c550c1e2a43ef4e0731c2be098ff
giluan13 <117103658+giluan13@users.noreply.github.com>
2022-12-16 20:11:14 -0500

    Delete EJERCICIO 2.py

--- EJERCICIO 2.py
+++
@@ -1,9 +0,0 @@
-import math
-def calcularAreaCirculo(r):
-    area = math.pi * math.pow(r, 2)
-    return area 
- 
-print("Ingrese radio del circulo")
-r = float(input())
-
-print("El area del circulo es " + str(calcularAreaCirculo(r)))
