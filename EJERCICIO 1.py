69b0a6111524f64c093e7a0fb880067f4eb22798
giluan13 <117103658+giluan13@users.noreply.github.com>
2022-12-16 20:11:03 -0500

    Delete EJERCICIO 1.py

--- EJERCICIO 1.py
+++
@@ -1,9 +0,0 @@
-def calcularArea(a, b):
-    area = (b* a) / 2 
-    return area
-print("Ingresar altura")
-a = float(input())
-print("ingresar base")
-b = float(input())
-
-print("El area es"  + str(calcularArea(a, b)))
